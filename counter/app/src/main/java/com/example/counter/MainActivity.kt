84623d55518292d497ad3564a027dd642a903f60
Gilberto Malaga <fullgamer494@hotmail.com>
2025-10-20 13:44:23 -0600

    Counter practice

---
+++ counter/app/src/main/java/com/example/counter/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.counter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.counter.ui.theme.CounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CounterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Content(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Content(innerPadding: PaddingValues){
+    var counter by remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.padding(innerPadding)
+    ){
+        Text("$counter", fontSize = 60.sp)
+
+        Row{
+            Button(
+                onClick = {counter--}
+            ){
+                Text("-")
+            }
+            Button(
+                onClick = {counter++}
+            ){
+                Text("+")
+            }
+        }
+    }
+}
